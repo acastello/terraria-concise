d3dd32447d86c538b8e38ade80b6d0012da668d6
Alejandro Castello <a.castello.pas@gmail.com>
2020-01-20 14:45:48 -0500

    Test 'addingItem'

--- test/GamepediaSpec/CoreSpec.hs
+++ test/GamepediaSpec/CoreSpec.hs
@@ -12,16 +12,34 @@ import Test.Hspec
 
 spec :: Spec
 spec = do
+
   describe "addingOrModifyingItem" $ do
     it "should never duplicate items by name" $ do
       flip evalStateT def $ do
-        return ()
         addingOrModifyingItem id "item" (either return (return . sampleItemByID))
+          % (`shouldBe` sampleItemByID 1)
         addingOrModifyingItem id "item" (either return (return . sampleItemByID))
           % (`shouldBe` sampleItemByID 1)
 
+  describe "addingItem" $ do
+    it "creates an item (by name) if it's not present already" $ do
+      flip evalStateT def $ do
+        addingItem id "item" (return . sampleItemByID)
+          % (`shouldBe` sampleItemByID 1)
+        addingItem id "item" (return . sampleItemByID)
+          % (`shouldBe` sampleItemByID 1)
+
+    it "doesn't update an existing item" $ do
+      flip evalStateT def $ do
+        addingItem id "item" (return . sampleItemByID)
+        addingItem id "item" (return . otherItemByID)
+            % (`shouldBe` sampleItemByID 1)
+
 sampleItemByID :: Int -> Item
 sampleItemByID iID = Item iID "item" (StaticResource "example") Nothing
+
+otherItemByID :: Int -> Item
+otherItemByID iID = Item iID "item" (StaticResource "otherExample") Nothing
 
 infixr 0 %
 (%) :: (MonadTrans t, Monad (t m), Monad m) => t m a -> (a -> m b) -> t m b
